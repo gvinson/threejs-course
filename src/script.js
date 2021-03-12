a1dbeb1170e3051cb38cd7e213151193b7e238d2
Grant Vinson <vinsongrant@gmail.com>
2021-03-12 12:53:17 -0600

    MeshStandardMaterial door

--- src/script.js
+++ src/script.js
@@ -1,19 +1,7 @@
 import './style.css'
 import * as THREE from 'three'
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
-
-/**
- * Notes
- *
- * PBR Materials = Physically based rendering:
- * Color: color applied on geometry
- * Displacement/Height Map = raise/lower vertices
- * Alpha = opacity (black 0, white 1)
- * Normal = like displacement, but no vertices subdivision, better performance than displacement, but the plane is still flat, usually PNG since we want really crisp pixels and PNG is losless compression
- * Ambient Occlusion = Grayscale image, add fake shadows to crevices - not physically accurate
- * Metalness = Grayscale image, white = shiny, black = flat, mostly for reflection
- * Roughness = Grayscale image, works in conjunction w/ Metalness, white is rough, black is smooth, mostly for light dissipation
- */
+import * as dat from 'dat.gui';
 
 /**
  * Base
@@ -25,48 +13,98 @@ const canvas = document.querySelector('canvas.webgl')
 const scene = new THREE.Scene()
 
 /**
- * Textures
+ * Debug
  */
-const loadingManager = new THREE.LoadingManager(); // used to know progress of loading assets - useful for a loader ui
-const textureLoader = new THREE.TextureLoader(loadingManager);
-const colorTexture = textureLoader.load('/textures/minecraft.png');
-const alphaTexture = textureLoader.load('/textures/door/alpha.jpg');
-const heightMap = textureLoader.load('/textures/door/height.jpg');
-const normalTexture = textureLoader.load('/textures/door/normal.jpg');
-const metalnessTexture = textureLoader.load('/textures/door/metalness.jpg');
-const roughnessTexture = textureLoader.load('/textures/door/roughness.jpg');
-const ambientTexture = textureLoader.load('/textures/door/ambientOcclusion.jpg');
-
-// colorTexture.repeat.x = 2;
-// colorTexture.repeat.y = 3;
-// colorTexture.wrapS = THREE.MirroredRepeatWrapping;
-// colorTexture.wrapT = THREE.MirroredRepeatWrapping;
-// colorTexture.offset.x = 0.5;
-// colorTexture.offset.y = 0.5;
-// colorTexture.rotation = Math.PI * 0.25;
-// colorTexture.center.x = 0.5;
-// colorTexture.center.y = 0.5;
-colorTexture.generateMipmaps = false;
-colorTexture.minFilter = THREE.NearestFilter;
-colorTexture.magFilter = THREE.NearestFilter;
+const gui = new dat.GUI();
 
 /**
- * Object
+ * Textures
  */
-const geometry = new THREE.BoxGeometry(1, 1, 1)
-const material = new THREE.MeshBasicMaterial({
-    map: colorTexture,
-    // displacementMap: heightMap,
-    // alphaMap: alphaTexture,
-    // normalMap: normalTexture,
-    // metalnessMap: metalnessTexture,
-    // roughnessMap: roughnessTexture,
-    // aoMap: ambientTexture,
-    // transparent: true,
-});
-const mesh = new THREE.Mesh(geometry, material)
-scene.add(mesh)
+const loadingManager = new THREE.LoadingManager();
+const textureLoader = new THREE.TextureLoader(loadingManager);
+const colorTexture = textureLoader.load('/textures/door/color.jpg');
+const alphaTexture = textureLoader.load('/textures/door/alpha.jpg');
+const aoTexture = textureLoader.load('/textures/door/ambientOcclusion.jpg');
+const heightTexture = textureLoader.load('/textures/door/height.jpg');
+const metalnessTexture = textureLoader.load('/textures/door/metalness.jpg');
+const normalTexture = textureLoader.load('/textures/door/normal.jpg');
+const roughnessTexture = textureLoader.load('/textures/door/roughness.jpg');
+const matcapTexture = textureLoader.load('/textures/matcaps/8.png');
+const gradientTexture = textureLoader.load('/textures/gradients/3.jpg');
+gradientTexture.minFilter = THREE.NearestFilter;
+gradientTexture.magFilter = THREE.NearestFilter;
+gradientTexture.generateMipmaps = false;
 
+/**
+ * Materials
+ */
+// const material = new THREE.MeshMatcapMaterial();
+// material.matcap = matcapTexture;
+
+// const material = new THREE.MeshDepthMaterial();
+// material.map = colorTexture;
+
+// const material = new THREE.MeshPhongMaterial(0xffffff, 0.5);
+// material.shininess = 100;
+// material.specular = new THREE.Color(0xff00ff);
+
+// const material = new THREE.MeshToonMaterial();
+// material.gradientMap = gradientTexture;
+
+const material = new THREE.MeshStandardMaterial({
+    map: colorTexture,
+    aoMap: aoTexture,
+    aoMapIntensity: 1,
+    displacementMap: heightTexture,
+    displacementScale: 0.05,
+    metalnessMap: metalnessTexture,
+    roughnessMap: roughnessTexture,
+    normalMap: normalTexture,
+    alphaMap: alphaTexture,
+    transparent: true,
+});
+material.normalScale.set(0.5, 0.5);
+const materialFolder = gui.addFolder('Material');
+materialFolder.add(material, 'metalness').min(0).max(1).step(0.0001);
+materialFolder.add(material, 'roughness').min(0).max(1).step(0.0001);
+materialFolder.add(material, 'aoMapIntensity').min(0).max(10).step(0.0001);
+materialFolder.add(material, 'displacementScale').min(0).max(10).step(0.0001);
+
+/**
+ * Lights
+ */
+const ambientLight = new THREE.AmbientLight(0xffffff, 0.5);
+scene.add(ambientLight);
+const pointLight = new THREE.PointLight(0xffffff, 0.5);
+pointLight.position.x = 2;
+pointLight.position.y = 3;
+pointLight.position.z = 4;
+scene.add(pointLight);
+
+/**
+ * Objects
+ */
+const plane = new THREE.Mesh(
+    new THREE.PlaneBufferGeometry(1, 1, 100, 100),
+    material,
+);
+// set uv2 so we can set the correct position for the alpha map
+plane.geometry.setAttribute(
+    'uv2',
+    new THREE.BufferAttribute(plane.geometry.attributes.uv.array, 2)
+);
+scene.add(plane);
+
+const sphere = new THREE.Mesh(
+    new THREE.SphereBufferGeometry(0.5, 64, 64),
+    material,
+);
+sphere.geometry.setAttribute(
+    'uv2',
+    new THREE.BufferAttribute(sphere.geometry.attributes.uv.array, 2)
+);
+sphere.position.x = -1.75;
+scene.add(sphere);
 /**
  * Sizes
  */
@@ -95,9 +133,9 @@ window.addEventListener('resize', () =>
  */
 // Base camera
 const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height, 0.1, 100)
-camera.position.x = 1
+camera.position.x = 0
 camera.position.y = 1
-camera.position.z = 1
+camera.position.z = 2
 scene.add(camera)
 
 // Controls
