f10ca97ba9231a6fb2400b2cea8fe8d52214e8f3
Grant Vinson <vinsongrant@gmail.com>
2021-03-18 11:11:56 -0500

    Finished

--- src/script.js
+++ src/script.js
@@ -16,106 +16,6 @@ const canvas = document.querySelector('canvas.webgl')
 const scene = new THREE.Scene()
 
 /**
- * Textures
- */
-const textureLoader = new THREE.TextureLoader()
-const particleTexture = textureLoader.load('/textures/particles/2.png');
-
-/**
- * Particles
- */
-const particlesGeometry = new THREE.BufferGeometry();
-const particlesMaterial = new THREE.PointsMaterial({
-    size: 0.1,
-    //color: new THREE.Color('#ff88cc'),
-    alphaMap: particleTexture,
-    transparent: true,
-    sizeAttenuation: true,
-});
-const count = 5000;
-
-// an array split like [0, 1, 2][3, 4, 5][6, 7, 8]... we need 3 items in the array (x, y, z) for each particles
-/**
- * Generate positions and points
- */
-const positions = new Float32Array(count * 3);
-const colors = new Float32Array(count * 3); // [r,g,b][r,g,b]
-
-// generate random positioning
-for (let i = 0; i < count * 3; i++) {
-    // keep # from -0.5 - 0.5, multiply by 10 to increase distance;
-    positions[i] = (Math.random() - 0.5) * 10;
-    // Generate random colors
-    colors[i] = Math.random();
-}
-
-// set custom position property on particles
-particlesGeometry.setAttribute(
-    'position',
-    new THREE.BufferAttribute(positions, 3), // 3 values for each vertix
-);
-
-particlesGeometry.setAttribute(
-    'color',
-    new THREE.BufferAttribute(colors, 3),
-);;
-
-/**
- *   Alpha pixel bug fix
- *   Sometimes the alpha map isn't good enough, it will have alpha z-index issues, causing semi-transparent boxes/borders around the shape.
- *
- * To fix this, we have a couple solutions:
- */
-
-/**
- * 1. alphaTest
- * This says to change the value for the alpha test from 0 (completely black) to 0.001 (99.999% black)
- */
-// particlesMaterial.alphaTest = 0.001;
-
-/**
- * 2. depthTest
- *
- * draw pixels no matter their z
- * - fix for alpha clipping bug, this will cause a bug if there are other objects in the scene w/ different color though
- */
-//particlesMaterial.depthTest = false;
-
-/**
- * 3. depthWrite
- *
- * The depth of what is being drawn is stored in a Depth Buffer
- * Instead of not testing if the particle is closer than whats in the depth buffer, we can tell the WebGL not to write the particles in that depth buffer with `depthTest`
- * - says not to draw particles that are in the depth buffer
- */
-// particlesMaterial.depthWrite = false;
-
-/**
- * 4. Additive Blending
- *
- * When we have multiple particles in th same pixel, the color gets really bright. The colors get combined - combinging red, blue and green creates a white color.
- *
- * Creates cool illuminating effect
- */
-particlesMaterial.depthWrite = false;
-particlesMaterial.blending = THREE.AdditiveBlending;
-particlesMaterial.vertexColors = true;
-
-/**
- * Alpha Testing Cube
- * Create cube to see bug effect w/ multiple colors/meshes
- */
-const cube = new THREE.Mesh(
-    new THREE.BoxBufferGeometry(),
-    new THREE.MeshBasicMaterial()
-);
-scene.add(cube);
-
-// Create particles
-const particles = new THREE.Points(particlesGeometry, particlesMaterial);
-scene.add(particles);
-
-/**
  * Sizes
  */
 const sizes = {
@@ -143,6 +43,8 @@ window.addEventListener('resize', () =>
  */
 // Base camera
 const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height, 0.1, 100)
+camera.position.x = 3
+camera.position.y = 3
 camera.position.z = 3
 scene.add(camera)
 
@@ -160,6 +62,144 @@ renderer.setSize(sizes.width, sizes.height)
 renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
 
 /**
+ * Galaxy
+ */
+const parameters = {
+    count: 100000,
+    size: 0.01,
+    radius: 5,
+    branches: 3,
+    spin: 1,
+    randomness: 0.2,
+    randomnessPower: 3,
+    insideColor: '#f56b16',
+    outsideColor: '#4b4be8',
+};
+
+// Set variables here so we can destroy/dispose of materials & points when changing values
+let geometry = null;
+let material = null;
+let points = null;
+
+const generateGalaxy = () => {
+
+    // Clear canvas for re-draw from GUI
+    if (points) {
+        geometry.dispose();
+        scene.remove(points);
+    }
+    if (material) {
+        material.dispose();
+    }
+
+    /**
+     * Geometry
+     */
+    geometry = new THREE.BufferGeometry();
+    const positions = new Float32Array(parameters.count * 3);
+
+    /**
+     * Colors
+     */
+    const colors = new Float32Array(parameters.count * 3); // [r,g,b][r,g,b]...
+    const colorInside = new THREE.Color(parameters.insideColor);
+    const colorOutside = new THREE.Color(parameters.outsideColor);
+
+    for (let i=0; i<parameters.count; i++) {
+        const i3 = i * 3;
+
+        // Radius of galax
+        // # from 0 -> 5 (or whatever radius is set to)
+        const radius = Math.random() * parameters.radius;
+
+        // Get what branch we want to place the point on
+        // Branch shape =
+        /*
+                  / 0
+                 /
+                /------------ 0.33
+                \
+                 \
+                  \ 0.66
+         */
+        // gets 0, 0.33, 0.66 = [x=0, y=0.33, z=0.66]
+        const branchAngel = (i % parameters.branches) / parameters.branches * Math.PI * 2;
+
+        // Calculate spin angel
+        const spinAngel = radius * parameters.spin;
+
+        // Randomness for each position = Math.pow(x, # of x's) - (2, 3) = 2 * 2 * 2 = 8
+        // we want to to do it for positive numbers though
+        const randomX = Math.pow(Math.random(), parameters.randomnessPower) * (Math.random() < 0.5 ? 1 : -1);
+        const randomY = Math.pow(Math.random(), parameters.randomnessPower) * (Math.random() < 0.5 ? 1 : -1);
+        const randomZ = Math.pow(Math.random(), parameters.randomnessPower) * (Math.random() < 0.5 ? 1 : -1);
+
+        // x, y & z positions for each geometry
+        positions[i3] = Math.cos(branchAngel + spinAngel) * radius + randomX;
+        positions[i3 + 1] = randomY;
+        positions[i3 + 2] = Math.sin(branchAngel + spinAngel) * radius + randomZ;
+
+        // colors
+        const mixedColor = colorInside.clone();
+        mixedColor.lerp(colorOutside, radius / parameters.radius);
+        colors[i3] = mixedColor.r;
+        colors[i3 + 1] = mixedColor.g;
+        colors[i3 + 2] = mixedColor.b;
+    }
+
+    geometry.setAttribute(
+        'position',
+        new THREE.BufferAttribute(positions, 3)
+    );
+
+    geometry.setAttribute(
+        'color',
+        new THREE.BufferAttribute(colors, 3)
+    );
+
+    /**
+     * Materials
+     */
+    material = new THREE.PointsMaterial({
+        size: parameters.size,
+        sizeAttenuation: true,
+        depthWrite: false,
+        blending: THREE.AdditiveBlending,
+        vertexColors: true,
+    });
+
+    /**
+     * Points
+     */
+    points = new THREE.Points(geometry, material);
+    scene.add(points);
+
+};
+generateGalaxy();
+
+/**
+ * GUI
+ */
+gui.add(parameters, 'count').min(0).max(100000).step(100)
+    .onFinishChange(generateGalaxy);
+gui.add(parameters, 'size').min(0.001).max(0.1).step(0.001)
+    .onFinishChange(generateGalaxy);
+gui.add(parameters, 'radius').min(0.01).max(20).step(0.001)
+    .onFinishChange(generateGalaxy);
+gui.add(parameters, 'branches').min(2).max(20).step(1)
+    .onFinishChange(generateGalaxy);
+gui.add(parameters, 'spin').min(-5).max(5).step(1)
+    .onFinishChange(generateGalaxy);
+gui.add(parameters, 'randomness').min(0).max(1).step(0.01)
+    .onFinishChange(generateGalaxy);
+gui.add(parameters, 'randomnessPower').min(0).max(10).step(0.5)
+    .onFinishChange(generateGalaxy);
+gui.addColor(parameters, 'insideColor')
+    .onFinishChange(generateGalaxy);
+gui.addColor(parameters, 'outsideColor')
+    .onFinishChange(generateGalaxy);
+
+/**
  * Animate
  */
 const clock = new THREE.Clock()
@@ -170,26 +210,6 @@ const tick = () =>
 
     // Update controls
     controls.update()
-
-    // Animate particles
-    for (let i=0; i < count; i++) {
-        // take each item in the array by 3 [x,y,z]
-        const i3 = i * 3;
-
-        // move particles with a sin() for x, y, and z
-        const x = particles.geometry.attributes.position.array[i3];
-        const y = particles.geometry.attributes.position.array[i3 + 1];
-        const z = particles.geometry.attributes.position.array[i3 + 2];
-
-        // animate y
-        // this position has POOR performance
-        // Best to use a custom shader by creating my own Points material
-        particles.geometry.attributes.position.array[i3 + 1] = Math.sin(elapsedTime + x);
-
-
-        // Tell ThreeJS the positions have changed
-        particles.geometry.attributes.position.needsUpdate = true;
-    }
 
     // Render
     renderer.render(scene, camera)
