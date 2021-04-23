0c740d6559ad1e9ed7c430a43323bef3ccde3712
Grant Vinson <vinsongrant@gmail.com>
2021-04-23 09:16:43 -0500

    Added multiple shader exapmles

--- src/script.js
+++ src/script.js
@@ -1,16 +1,15 @@
 import './style.css'
 import * as THREE from 'three'
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
-import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader.js'
-import { RGBELoader } from "three/examples/jsm/loaders/RGBELoader";
 import * as dat from 'dat.gui'
+import testVertexShader from './shaders/test/vertex.glsl'
+import testFragmentShader from './shaders/test/fragment.glsl'
 
 /**
  * Base
  */
 // Debug
 const gui = new dat.GUI()
-const debugObject = {}
 
 // Canvas
 const canvas = document.querySelector('canvas.webgl')
@@ -19,10 +18,25 @@ const canvas = document.querySelector('canvas.webgl')
 const scene = new THREE.Scene()
 
 /**
- * Loaders
+ * Test mesh
  */
-const gltfLoader = new GLTFLoader()
-const cubeTextureLoader = new THREE.CubeTextureLoader()
+const clock = new THREE.Clock();
+// Geometry
+const geometry = new THREE.PlaneGeometry(1, 1, 32, 32)
+
+// Material
+const material = new THREE.ShaderMaterial({
+    vertexShader: testVertexShader,
+    fragmentShader: testFragmentShader,
+    side: THREE.DoubleSide,
+    uniforms: {
+        uTime: { value: clock.getElapsedTime()}
+    }
+});
+
+// Mesh
+const mesh = new THREE.Mesh(geometry, material)
+scene.add(mesh);
 
 /**
  * Sizes
@@ -31,136 +45,6 @@ const sizes = {
     width: window.innerWidth,
     height: window.innerHeight
 }
-
-/**
- * Camera
- */
-// Base camera
-const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height, 0.1, 100)
-camera.position.set(2, 2, 2)
-scene.add(camera)
-
-// Controls
-const controls = new OrbitControls(camera, canvas)
-controls.target.set(0, 0.75, 0)
-controls.enableDamping = true
-
-/**
- * Renderer
- */
-const renderer = new THREE.WebGLRenderer({
-    canvas: canvas,
-    antialias: true
-})
-renderer.physicallyCorrectLights = true
-renderer.outputEncoding = THREE.sRGBEncoding
-renderer.toneMapping = THREE.ReinhardToneMapping
-renderer.toneMappingExposure = 0.955
-renderer.shadowMap.enabled = true
-renderer.shadowMap.type = THREE.PCFSoftShadowMap
-renderer.setSize(sizes.width, sizes.height)
-renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
-gui
-    .add(renderer, 'toneMapping', {
-        No: THREE.NoToneMapping,
-        Linear: THREE.LinearToneMapping,
-        Reinhard: THREE.ReinhardToneMapping,
-        Cineon: THREE.CineonToneMapping,
-        ACESFilmic: THREE.ACESFilmicToneMapping
-    })
-    .onFinishChange(() =>
-    {
-        renderer.toneMapping = Number(renderer.toneMapping)
-        updateAllMaterials()
-    })
-gui.add(renderer, 'toneMappingExposure').min(0).max(10).step(0.001)
-
-const pmremGenerator = new THREE.PMREMGenerator( renderer );
-pmremGenerator.compileEquirectangularShader();
-
-
-/**
- * Lights
- */
-const topLight = new THREE.RectAreaLight(0xffffff, 10, 20, 20);
-topLight.position.set(0, 2, 0);
-scene.add(topLight);
-
-const sideLight = new THREE.RectAreaLight(0xffffff, 5, 10, 20);
-sideLight.position.set(-5, 0, 5);
-scene.add(sideLight);
-
-gui.addFolder('lights');
-gui.add(topLight.position, 'x').min(-5).max(10);
-gui.add(topLight.position, 'y').min(-5).max(10);
-gui.add(topLight.position, 'z').min(-5).max(10);
-gui.add(topLight, 'intensity').min(0).max(100).step(0.01);
-gui.add(sideLight.position, 'x').min(-5).max(10);
-gui.add(sideLight.position, 'y').min(-5).max(10);
-gui.add(sideLight.position, 'z').min(-5).max(10);
-gui.add(sideLight, 'intensity').min(0).max(100).step(0.01);
-
-// light.position.set(0, 0.8, 1.5);
-// //light.castShadow = true
-// light.shadow.camera.far = 15
-// light.shadow.mapSize.set(1024, 1024)
-// //light.shadow.normalBias = 0.05
-// scene.add(light);
-// gui.addFolder('lights');
-// gui.add(light.position, 'x').min(-5).max(10);
-// gui.add(light.position, 'y').min(-5).max(10);
-// gui.add(light.position, 'z').min(-5).max(10);
-// gui.add(light, 'intensity').min(0).max(100).step(0.01).name('lightIntensity');
-
-/**
- * Update all materials
- */
-const updateAllMaterials = () =>
-{
-    scene.traverse((child) =>
-    {
-        if(child instanceof THREE.Mesh && child.material instanceof THREE.MeshStandardMaterial)
-        {
-            //child.material.envMapIntensity = debugObject.envMapIntensity
-            child.material.needsUpdate = true
-            child.castShadow = true
-            child.receiveShadow = true
-        }
-    })
-}
-
-/**
- * Environment map
- */
-const newLoader = new RGBELoader();
-let environmentMap = null;
-newLoader.load( '/textures/3.hdr', function ( texture ) {
-    const envMap = pmremGenerator.fromEquirectangular( texture ).texture;
-    scene.environment = envMap;
-    texture.dispose();
-    pmremGenerator.dispose();
-    environmentMap = envMap;
-});
-debugObject.envMapIntensity = 0.305;
-gui.add(debugObject, 'envMapIntensity').min(0).max(10).step(0.001).onChange(updateAllMaterials)
-
-/**
- * Models
- */
-let mixer = null;
-gltfLoader.load('/models/model.glb',
-    (gltf) => {
-        mixer = new THREE.AnimationMixer(gltf.scene);
-
-        const visor = mixer.clipAction(gltf.animations[0]);
-        visor.play();
-
-        gltf.scene.position.y = 1;
-        scene.add(gltf.scene)
-
-        updateAllMaterials()
-    },
-)
 
 window.addEventListener('resize', () =>
 {
@@ -178,23 +62,33 @@ window.addEventListener('resize', () =>
 })
 
 /**
+ * Camera
+ */
+// Base camera
+const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height, 0.1, 100)
+camera.position.set(0.25, - 0.25, 1)
+scene.add(camera)
+
+// Controls
+const controls = new OrbitControls(camera, canvas)
+controls.enableDamping = true
+
+/**
+ * Renderer
+ */
+const renderer = new THREE.WebGLRenderer({
+    canvas: canvas
+})
+renderer.setSize(sizes.width, sizes.height)
+renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
+
+/**
  * Animate
  */
-const clock = new THREE.Clock()
-let previousTime = 0
-
 const tick = () =>
 {
-    const elapsedTime = clock.getElapsedTime()
-    const deltaTime = elapsedTime - previousTime
-    previousTime = elapsedTime
-
     // Update controls
     controls.update()
-
-    if (mixer) {
-        mixer.update(deltaTime);
-    }
 
     // Render
     renderer.render(scene, camera)
